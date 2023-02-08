4fecc32d3264919aec314d9e6021f70a2aad2193
jagannathareddy <nerusupallijagannathareddy72@gmail.com>
2023-02-08 08:53:58 +0000

    router 1 solution

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,36 @@
-// Write your JS code here
 import {Link} from 'react-router-dom'
+
 import './index.css'
 
 const Header = () => (
-  <div className="header-container">
-    <div className="bg-container">
-      <nav className="nav-header">
-        <div className="nav-container">
-          <div className="logo-container">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
-              alt="wave"
-              className="wave"
-            />
-            <h1 className="wave-heading">Wave</h1>
-          </div>
-          <ul className="nav-menu">
-            <li>
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+  <nav className="header-container">
+    <div className="logo-and-title-container">
+      <img
+        alt="wave"
+        className="logo"
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+      />
+      <h1 className="title">Wave</h1>
     </div>
-  </div>
+
+    <ul className="nav-items-list">
+      <li className="link-item">
+        <Link className="route-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li className="link-item">
+        <Link className="route-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li className="link-item">
+        <Link className="route-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
 )
+
 export default Header
